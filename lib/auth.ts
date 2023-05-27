bef6a08b2b29aeb76103b346632dbf1983646ff3
Mohammed Ahmed <mohammed_ahmed4949@yahoo.com>
2023-05-27 05:23:30 +0300

    Finished register and sign in API routes

---
+++ lib/auth.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import { SignJWT, jwtVerify } from "jose";
+import { db } from "./db";
+
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+
+export const comparePasswords = (
+  plainPassword: string,
+  hashedPassword: string
+) => bcrypt.compare(plainPassword, hashedPassword);
+
+export const createJWT = (user: { id: string; email: string }) => {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setIssuedAt(iat)
+    .setExpirationTime(exp)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+};
+
+export const validateJWT = async (jwt: string) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as any;
+};
+
+export const getUserFromCookie = async (cookies) => {
+  const jwt = cookies.get(process.env.COOKIE_NAME);
+  const { id } = await validateJWT(jwt);
+
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return user;
+};
